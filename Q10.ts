eb7b8bf51f05771df2136ff1f463ef2230a3266c
Novish Khalil <novishkhalil2@hotmail.com>
2024-11-11 00:14:25 +0500

    Add files via upload

---
+++ Q10.ts
@@ -0,0 +1,9 @@
+//10.Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+//My NAME: Novish Khalil
+//Current Date: 11/1/2024
+let num1 = 5;
+let num2 = 10;
+let sum = num1 + num2;
+console.log(`The sum of ${num1} and ${num2} is ${sum}`);
+//in this program we use the  let to declare variables num1, num2 and sum.
+//then calculate the sum of num1 and num2 and disply the result
